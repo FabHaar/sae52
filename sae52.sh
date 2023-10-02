0a3cb56d562ef2e7c822d50eaefcb97c294ab12a
FabHaar <fabienhaar@gmail.com>
2023-10-02 10:46:44 -0400

    update sae52.sh

--- sae52.sh
+++ sae52.sh
@@ -7,6 +7,6 @@ docker volume create nginx-logs
 
 docker run --name nginx-server -p 80:80 -v nginx-logs:/var/log/nginx -d serveur-nginx
 
-docker run --name nginx-logs-collector -v nginx-logs:/var/log/nginx -d -it go-access
+docker run --name logs-collector -v nginx-logs:/var/log/nginx -d -it go-access
 
-docker exec -it nginx-logs-collector bash -c "goaccess -f /var/log/nginx/access.log"
+docker exec -it logs-collector bash -c "goaccess -f /var/log/nginx/access.log"
